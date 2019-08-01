e56949fcbf4784b7059c820a87d4733f4a01dd07
Jesus R <arodriguez@arisoft.com.mx>
2019-07-31 18:50:40 -0600

    crea vista movimiento

--- app/src/main/java/app/arisoft_app/movimientosInventario.java
+++ app/src/main/java/app/arisoft_app/movimientosInventario.java
@@ -1,19 +1,26 @@
 package app.arisoft_app;
 
+import android.app.DatePickerDialog;
+import android.app.FragmentTransaction;
 import android.app.ProgressDialog;
 import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,24 +36,32 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Calendar;
+import java.util.Date;
 
 import app.arisoft_app.Tools.Database;
 
-public class movimientosInventario extends AppCompatActivity {
+public class movimientosInventario extends AppCompatActivity implements DialogInterface.OnDismissListener {
     Button btn_mov;
-    EditText et_mov;
-    TextView tv_mov;
+    ImageButton btn_buscarcod;
+    EditText et_mov,et_codigo;
+    TextView tv_mov,tv_fecha;
     private String URL;
     String codigo,descripcion,contenido="";
     ProgressDialog progreso;
+    private DatePickerDialog.OnDateSetListener datesl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movimientos_inventario);
         et_mov=(EditText)findViewById(R.id.et_mov);
+        et_codigo=(EditText)findViewById(R.id.et_codigo);
         tv_mov=(TextView) findViewById(R.id.tv_mov);
+        tv_fecha=(TextView) findViewById(R.id.tv_fecha);
+        btn_buscarcod=(ImageButton)findViewById(R.id.btn_buscarcod);
         getDomain();
+        fechaActual();
 
         et_mov.addTextChangedListener(new TextWatcher() {
             @Override
@@ -67,6 +82,35 @@ public class movimientosInventario extends AppCompatActivity {
 
             }
         });
+
+        btn_buscarcod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"click",Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+        datesl=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month=month+1;
+                String fecha="";
+                //CharSequence s = DateFormat.format("dd-MM-yyyy",fecha);
+                if(month<10)
+                {
+                    fecha=""+dayOfMonth+"-0"+month+"-"+year;
+                }
+                else
+                {
+                    fecha=""+dayOfMonth+"-"+month+"-"+year;
+                }
+
+                tv_fecha.setText(fecha);
+
+            }
+        };
     }
     public void buscar(View view)
     {
@@ -74,6 +118,42 @@ public class movimientosInventario extends AppCompatActivity {
         String ruta=espaciosBlanco(et_mov.getText().toString().trim());
         //Toast.makeText(getApplicationContext(),""+ruta,Toast.LENGTH_SHORT).show();
         new cargarArticulo().execute(ruta);
+    }
+
+    public void agregar(View view)
+    {
+
+    }
+    public void comentario(View view)
+    {
+
+    }
+    public void guardar(View view)
+    {
+
+    }
+    public void fecha(View view)
+    {
+        Calendar cal= Calendar.getInstance();
+        int año=cal.get(Calendar.YEAR);
+        int mes=cal.get(Calendar.MONTH);
+        int dia=cal.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dialog= new DatePickerDialog(
+                this,
+                android.R.style.Theme_Holo_Dialog_MinWidth,
+                datesl,
+                año,mes,dia
+        );
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+
+    }
+    public void fechaActual()
+    {
+        Date d = new Date();
+        CharSequence s = DateFormat.format("dd-MM-yyyy", d.getTime());
+        tv_fecha.setText(s);
     }
     public String espaciosBlanco(String cadena)
     {
@@ -93,6 +173,11 @@ public class movimientosInventario extends AppCompatActivity {
         //Toast.makeText(getApplicationContext(),""+x,Toast.LENGTH_SHORT).show();
 
         return x;
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+
     }
 
     //PETICION FACTURAS ASYNC TASK
